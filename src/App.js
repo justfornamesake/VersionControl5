604eface51c44fb9478c7884abdaa460b8fcca52
satya_jha <satyajeetjha06@gmail.com>
2019-03-05 21:06:03 +0530

    pushing

--- src/App.js
+++ src/App.js
@@ -7,18 +7,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <ul>
+            <li> One </li>
+            <li> Two</li>
+            <li> Three</li>
+            </ul>
           <p>This is new paragraph introduced </p>
           <p>Change in master branch </p>
         </header>
